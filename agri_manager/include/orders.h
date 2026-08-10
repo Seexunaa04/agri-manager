86563629bf1818cc8d9526189731de8f8d547777
Seexunaa04 <amarcheikhouna8@gmail.com>
2026-08-11 00:43:56 +0200

    Fix header guard in orders.h

--- agri_manager/include/orders.h
+++ agri_manager/include/orders.h
@@ -11,4 +11,4 @@ int find_order_by_id(int id, Order *out_order);
 int get_next_order_id(void);
 void generate_order_receipt(const Order *ord, const User *usr, const Product *prd);
 
-#endif // ORDERS_H
+#endif 
